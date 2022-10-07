8e372ebe67e21ca94070040df0387fbcb4009c8c
nicolessilva <nicolesilvaon@gmail.com>
2022-10-07 18:11:20 -0300

    adjustment button left

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -18,11 +18,18 @@ class _LoginPageState extends State<LoginPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const SizedBox(height: 100.0),
-            IconButton(
-              onPressed: () {},
-              icon: const Icon(Icons.keyboard_arrow_left),
+            const SizedBox(height: 70.0),
+            Container(
+              width: 40.0,
+              height: 40.0,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(12.0),
+                  border: Border.all(
+                    color: const Color(0xFFE8ECF4),
+                  )),
+              child: const Icon(Icons.keyboard_arrow_left),
             ),
+            const SizedBox(height: 28.0),
             const Text(
               'Welcome back! Glad\nto see you, Again!',
               style: TextStyle(
